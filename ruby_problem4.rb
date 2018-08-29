82355929027a96fe362f5f52c4cdbdb0fdd918ed
Micah Caffey <mrcaffey@gmail.com>
2018-08-29 17:14:09 -0600

    output longest word in a string

--- ruby_problem4.rb
+++ ruby_problem4.rb
@@ -1,9 +1,9 @@
 # Write a script that accepts a string as a parameter and find the longest word within the string.
 
 def longestword
-
-
-  
+  puts "Type a sentence. The program will output the longest word within your string."
+  input = gets.to_s.strip
+  puts input.split(" ").max_by(&:length)
 end
 
 longestword
